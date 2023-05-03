a0c2034247487e54308f30a5bb0672f88e36be12
Dojelin <dojelin@gmail.com>
2023-05-03 14:52:41 -0400

    Create service for trainings and implement it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { StopTrainingComponent } from './training/current-training/stop-traing.c
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { TrainingComponent } from './training/training.component';
+import { TrainingService } from './training/training.service';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
@@ -40,10 +41,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, TrainingService],
   bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
+  entryComponents: [StopTrainingComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,16 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
+@Injectable()
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent {
+  exercises: Exercise[] = [];
 
-  @Output() trainingStart = new EventEmitter<void>();
-
-  onStartTraining(){
-    this.trainingStart.emit();
+  constructor(private trainingService: TrainingService) {
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
+  }
 }
