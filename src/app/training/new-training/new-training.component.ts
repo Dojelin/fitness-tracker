b6ed70bf3f02219531e5a2c9e963216b75b813be
Dojelin <dojelin@gmail.com>
2023-05-08 13:42:55 -0400

    add Id from farebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,7 @@ import { Component, Injectable, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TrainingService } from '../training.service';
 
 @Injectable()
@@ -19,7 +20,21 @@ export class NewTrainingComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return docArray.map((doc) => {
+            return {
+              id: doc.payload.doc.id,
+              name: doc.payload.doc.data()['name'],
+              calories: doc.payload.doc.data()['calories'],
+              duration: doc.payload.doc.data()['duration'],
+            };
+          });
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
